ec1ac2f710d2a61bd25a94846b7885093d047e85
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-03-30 04:59:22 -0400

    external method, internal header, max helper

--- BinRadix.java
+++ BinRadix.java
@@ -1,8 +1,20 @@
+import java.util.*;
 public class BinRadix{
   public static void main(String[] args){
 
   }
   public static void binradixsort(int[] data){
+    int top = max(data); //one linear pass
+    sortH(data,top,0);
+  }
+  private static void sortH(int[] data,int top,int place){
 
+  }
+  public static int max(int[] data){
+    int out = data[0];
+    for(int n : data){
+      if(n>out) out = n;
+    }
+    return out;
   }
 }
