e4aaa4cda6dfe229813a02043513ac1a568d0423
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-04-02 09:54:51 -0400

    headers of sub-methods, psuedocode at high level

--- Radix.java
+++ Radix.java
@@ -3,6 +3,31 @@ public class Radix{
 
   }
   public static void radixsort(int[] data){
-    
+    int maxVal = max(data);
+    MyLinkedList<Integer> modifData = arrayToMLL(data);
+    radix(modifData,0,maxVal);
+  }
+  private static void radix(MyLinkedList<Integer> data, int pow,int maxVal){
+    if(Math.pow(10,pow) <= maxVal){
+      @SuppressWarnings("unchecked")
+      MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+      while(data.size() > 0){
+        addToBucket(buckets,data.remove(0));
+      }
+      //merge buckets
+    }
+  }
+  private static int max(int[] data){
+    return -1;
+  }
+  private static MyLinkedList<Integer> arrayToMLL(int[] data){
+    MyLinkedList<Integer> out = new MyLinkedList<Integer>();
+    for(int val : data){
+      out.add(val);
+    }
+    return out;
+  }
+  private static void addToBucket(MyLinkedList<Integer>[] buckets,int val){
+
   }
 }
