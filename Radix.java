2beda898e47ace8df31dc1461a4d9630211c18e0
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-04-02 10:11:34 -0400

    theoretical mergeBuckets and MLLtoArray

--- Radix.java
+++ Radix.java
@@ -6,14 +6,18 @@ public class Radix{
     int maxVal = max(data);
     MyLinkedList<Integer> modifData = arrayToMLL(data);
     radix(modifData,0,maxVal);
+    data = MLLtoArray(modifData);
   }
   private static void radix(MyLinkedList<Integer> data, int pow,int maxVal){
     if(Math.pow(10,pow) <= maxVal){
       MyLinkedList<Integer>[] buckets = new MyLinkedList[10];
+      for(int i=0;i<buckets.length;i++){
+        buckets[i] = new MyLinkedList<Integer>();
+      }
       while(data.size() > 0){
         addToBucket(buckets,data.remove(0),pow);
       }
-      //merge buckets
+      data = mergeBuckets(buckets);
     }
   }
   private static int max(int[] data){
@@ -33,5 +37,20 @@ public class Radix{
   private static void addToBucket(MyLinkedList<Integer>[] buckets,int val, int pow){
     int place = ((val % (int)(Math.pow(10,pow+1))) / (int)(Math.pow(10,pow)));
     buckets[place].add(val);
+  }
+  private static MyLinkedList<Integer> mergeBuckets(MyLinkedList<Integer>[] buckets){
+    MyLinkedList<Integer> out = buckets[0];
+    for(int i=1;i<buckets.length;i++){
+      out.extend(buckets[i]);
+    }
+    return out;
+  }
+  private static int[] MLLtoArray(MyLinkedList<Integer> data){
+    int[] out = new int[data.size()];
+    int i = 0;
+    while(data.size() > 0){
+      out[i++] = data.removeFront();
+    }
+    return out;
   }
 }
