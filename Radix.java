4b399a687a21b2ae1d75dcd487ccd76eab2caef8
kiran-vuksanaj <mvuksanaj00@stuy.edu>
2019-04-03 18:41:31 -0400

    corrected typo:binary radix seeming to work

--- Radix.java
+++ Radix.java
@@ -1,8 +1,8 @@
 import java.util.Arrays;
 public class Radix{
   public static void main(String[] args){
-    int[] briefTest = {4, 2, 3, 4,12,6,798,2,2,4, 4, 7, 8 ,2};
-    radixsort(briefTest);
+    int[] briefTest = {9,8,7,6,5,4,3,2,1};
+    radixsortBin(briefTest);
     System.out.println(Arrays.toString(briefTest));
   }
   //binary radixsort
@@ -11,18 +11,21 @@ public class Radix{
     radixbin(data,1,maxVal);
   }
   private static void radixbin(int[] data,int place,int maxVal){
+    //System.out.println(place+": "+Arrays.toString(data));
     if(place <= maxVal){
       //1. sort at this level
       int[] temp = new int[data.length];//temporary necessary bc back end goes in backwards
       int s = 0;
       int e = data.length;
       for(int val : data){
+        //System.out.println("\t"+Arrays.toString(temp));
         if( (val & place) == 0 ){
           temp[s++] = val; //leaves s in empty space to write
         }else{
           temp[--e] = val; //leaves e in a space just used
         }
       }
+      //System.out.println("\t"+Arrays.toString(temp));
       //at this point: temp has two buckets
       //    temp[:s] - 0 at this place value
       //    temp[e:] - 1 at this place value, but values reversed
@@ -31,7 +34,8 @@ public class Radix{
         data[i] = temp[i];
       }
       for(int i=0; i+e < data.length ; i++){
-        data[data.length - 1] = temp[i+e];
+        data[data.length - 1 - i] = temp[i+e];
+        //System.out.println("\t~"+Arrays.toString(data));
       }
       //2. recurse up
       radixbin(data,place << 1,maxVal);
